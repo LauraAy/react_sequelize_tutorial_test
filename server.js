3f1fd27ecc447f32dd61a3a8bcbf741205f3e1cb
LauraAy <laura.ay1564@gmail.com>
2022-06-08 15:41:50 -0700

    created db

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Aydelotte application." });
 });
+const db = require("./app/models");
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log('Server is running on port ${Port}.');
 });
 
-const app = express();
-app.use(...);
-const db = require("./app/models");
-db.sequelize.sync();
-//db.sequelize.sync({ force: true }).then(() => {console.log("Drop and re-sync db.");})
