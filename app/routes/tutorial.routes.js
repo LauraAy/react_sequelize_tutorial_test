4f1148bbbeaf20d99019b79167fa5c77dae373f2
LauraAy <laura.ay1564@gmail.com>
2022-06-09 15:49:24 -0700

    revert to e27ac9e

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -8,9 +8,7 @@ module.exports = app => {
   //Retrieve all published Tutorials
   router.get("/published", tutorials.findAllPublished);
   //Retrieve a single Tutorial with id
-  router.get("/:id", tutorials.findone);
-  //Update a Tutorial with id
-  router.put("/:id", tutorials.update);
+  router.get("/:id", tutorials.update);
   //Delete a Tutorial with id
   router.delete("/:id", tutorials.delete);
   //Delete all Tutorials
